6fc8e3d2fd190668e420e4d9009ba09db4a1666d
agoalofalife <agoalofalife@gmail.com>
2018-04-17 12:18:29 +0300

    add package depend react-native-elements

--- App.js
+++ App.js
@@ -6,32 +6,25 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import ListBooksComponent from './js/components/listBooks/index';
+
+import { SearchBar } from 'react-native-elements'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+          <SearchBar
+              showLoading
+              placeholder='Type Here...' />
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          Welcome to Love book!
         </Text>
       </View>
     );
