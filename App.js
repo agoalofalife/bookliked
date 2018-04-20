52adda4595d4009ad056eabdbae3469911fdea54
agoalofalife <agoalofalife@gmail.com>
2018-04-20 17:06:59 +0300

    add async storage

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
     View,
+    AsyncStorage,
 } from 'react-native';
 
 import StartPageComponent from './ui/components/StartPageComponent'
@@ -9,7 +10,16 @@ import StartPageComponent from './ui/components/StartPageComponent'
 export default class App extends Component{
   constructor(){
     super();
+    this.state = {
+        token:null
+    }
   }
+
+   async componentDidMount(){
+       let token = await AsyncStorage.getItem('token');
+       this.setState({token})
+    }
+
   render() {
     return (
       <View style={styles.container}>
