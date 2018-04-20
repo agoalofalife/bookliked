2eae6c9cebdcc545115505da17f672c1891975c5
agoalofalife <agoalofalife@gmail.com>
2018-04-20 16:07:16 +0300

    add new fonts

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View,
+    View,
     Text,
+    Image,
+    Dimensions
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ListBooksComponent from './js/components/listBooks/index';
@@ -10,6 +12,14 @@ import { SearchBar, Input } from 'react-native-elements'
 import Login from './js/components/login/index'
 
 import Book from './js/components/book/index'
+
+
+const {height, width} = Dimensions.get('window');
+const widthLogoBananas = 230;
+const heightLogoBananas = 230;
+const marginLeftBananas = (width - widthLogoBananas)/2;
+const marginTopBananas = (height - heightLogoBananas)/8;
+
 type Props = {};
 
 export default class App extends Component<Props> {
@@ -33,6 +43,10 @@ export default class App extends Component<Props> {
       };
     return (
       <View style={styles.container}>
+          <View style={{backgroundColor:"#fce82f", flex:1}}>
+            <Text style={{ fontSize: 40, color:'black', textAlign: 'center', marginTop:30, fontFamily: "Nexa_Script_Heavy"}}>Bananas</Text>
+              <Image source={require('./assets/images/bananas-icon.png')}  style={{marginLeft:marginLeftBananas,marginTop:marginTopBananas,width: widthLogoBananas, height: heightLogoBananas, resizeMode: Image.resizeMode.contain,}} />
+          </View>
           {/*{this.state.isAuth &&*/}
               {/*<SearchBar*/}
               {/*showLoading*/}
@@ -43,7 +57,7 @@ export default class App extends Component<Props> {
               {/*placeholder='Какая книга?'/>}*/}
 
           {/*{this.state.isAuth && <ListBooksComponent/>}*/}
-          {!this.state.isAuth && <Login changeAuth={this.setAuth}/>}
+          {/*{!this.state.isAuth && <Login changeAuth={this.setAuth}/>}*/}
       </View>
     );
   }
